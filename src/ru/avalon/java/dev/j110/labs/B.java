464e6f69250e28d90dd59dc6bba0e659a3c75cb1
Nikolai Shilenko <nick.schilenko@gmail.com>
2020-11-05 19:20:47 +0400

    Add class B

---
+++ src/ru/avalon/java/dev/j110/labs/B.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ru.avalon.java.dev.j110.labs;
+
+/**
+ * @author Nikolai Shilenko
+ *
+ */
+public class B {
+	public static A a;
+	private A field;
+	
+	public static void a() {
+	}
+	
+	protected void a(A arg) {
+	}
+}
